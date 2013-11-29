fd35a3dc4dad241413e1537c980bbbb5740ff494
sumiaowen <sumiaowen@gmail.com>
2013-11-29 21:37:01 +0800

    add validate form data file

---
+++ validate-form-data/validate.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sumiaowen
+ * Date: 13-11-28
+ * Time: 下午3:55
+ * To change this template use File | Settings | File Templates.
+ */
+
+//表单数据安全验证：
+
+//1、数据类型转换，如：(int)、(string)......
+$pageNum = (int)$_GET['page'];	//页码
+
+//2、验证数据长度
+
+//3、正则判断
+
+//4、数据转义
+
+//5、特殊字符替换
+
+//6、格式化数据，如:strip_tags
+
+//7、表单域名和数据库字段名不同
+
+//8、防止远程表单提交
