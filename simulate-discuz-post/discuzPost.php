3a76f600346b06d12b42cd03b07d96420a0fe4e9
sumiaowen <sumiaowen@gmail.com>
2013-12-02 20:15:41 +0800

    模拟DISCUZ发帖

---
+++ simulate-discuz-post/discuzPost.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * 模拟discuz发贴
+ * Created by PhpStorm.
+ * User: sumiaowen
+ * Contact: http://www.php230.com/
+ * Date: 13-12-2
+ * Time: 下午8:14
+ * To change this template use File | Settings | File Templates.
+ */ 
