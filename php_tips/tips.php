449500c0bb2ea733eb55839d197d5ffbe28e8576
sumiaowen <sumiaowen@gmail.com>
2013-12-03 23:03:02 +0800

    add php tips

---
+++ php_tips/tips.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * 记录PHP 小技巧
+ * Created by PhpStorm.
+ * User: sumiaowen
+ * Contact: http://www.php230.com/
+ * Date: 13-12-3
+ * Time: 下午10:55
+ * To change this template use File | Settings | File Templates.
+ */
+
+//1、in_array()函数，该函数会把值转换成整型后，再做比较，建议使用时加上第三个参数
+
+//该例子将输出：one
+if(in_array('01', array('1')))
+{
+	echo 'one';
+}
+elseif(in_array('01', array('1'), true))
+{
+	echo 'two';
+}
+
+function daYin($param)
+{
+	echo '<pre>';
+	var_dump($param);
+	echo '</pre><hr>';
+}
