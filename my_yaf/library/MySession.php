8725426fc0e0357c0471ced6c85efd0053a68ab5
sumiaowen <sumiaowen@gmail.com>
2013-12-08 20:23:21 +0800

    add session file

---
+++ my_yaf/library/MySession.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * 简单 session 操作类
+ * Created by PhpStorm.
+ * User: sumiaowen
+ * Contact: http://www.php230.com/
+ * Date: 13-12-8
+ * Time: 下午8:22
+ * To change this template use File | Settings | File Templates.
+ */ 
