5333e6ae6fc24ee3230e3d20d7b51ee5e579359d
sumiaowen <sumiaowen@gmail.com>
2013-12-10 15:25:11 +0800

    modify yaf framwork

--- my_yaf/index.php
+++ my_yaf/index.php
@@ -2,7 +2,6 @@
 
 define('APPLICATION_PATH', dirname(__FILE__));
 
-$application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
+$application = new Yaf_Application(APPLICATION_PATH . "/conf/application.ini");
 
 $application->bootstrap()->run();
-?>
