10841bd6c857fa788361b10defc16215b7ba6c00
sumiaowen <sumiaowen@gmail.com>
2013-12-11 15:09:53 +0800

    add php mongodb class file

---
+++ php-mongodb/mongodb.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * php mongodb 操作
+ * Created by PhpStorm.
+ * User: sumiaowen
+ * Date: 13-12-11
+ * Time: 下午3:09
+ * To change this template use File | Settings | File Templates.
+ */ 
