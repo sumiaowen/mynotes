6db3b4b5c3b6b889aed3919a8d9fa05078e5c054
sumiaowen <sumiaowen@gmail.com>
2013-12-20 14:56:59 +0800

    add php redis option class

---
+++ my_yaf/library/MyRedis.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * redis 简单操作类
+ * Created by PhpStorm.
+ * User: sumiaowen
+ * Date: 13-12-20
+ * Time: 下午2:54
+ * To change this template use File | Settings | File Templates.
+ */
+class MyRedis
+{
+
+}
