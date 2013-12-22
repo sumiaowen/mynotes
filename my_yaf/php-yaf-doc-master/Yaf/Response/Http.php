5b51ba0c04437a1a142f6ebd064955d0e2989109
sumiaowen <sumiaowen@gmail.com>
2013-12-22 23:49:03 +0800

    add yaf doc file in project

---
+++ my_yaf/php-yaf-doc-master/Yaf/Response/Http.php
@@ -0,0 +1,23 @@
+<?php
+namespace Yaf\Response ;
+
+/**
+ *
+ */
+class Http extends \Yaf\Response_Abstract {
+
+	/**
+	 * @var int
+	 */
+	protected $_response_code = 200;
+
+	/**
+	 *
+	 */
+	private function __clone(){ }
+
+	/**
+	 * @return string
+	 */
+	private function __toString(){ }
+}
